b0ebdb52ea4db0cf2217e12d008bd0f960df26cc
LuguDev <106494315+Lufredy123xd@users.noreply.github.com>
2024-06-26 16:31:54 -0300

    Carpeta Enums

---
+++ Laboratorio-3/src/Enums/Enum.cpp
@@ -0,0 +1,18 @@
+/*
+ * Enum.cpp
+ *
+ *  Created on: 26 jun. 2024
+ *      Author: Windows-User
+ */
+
+#include "Enum.h"
+
+Enum::Enum() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Enum::~Enum() {
+	// TODO Auto-generated destructor stub
+}
+
